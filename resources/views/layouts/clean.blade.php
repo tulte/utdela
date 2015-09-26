72718e530be210ef07661a2ad26912802c01dd03
tulte <tulte@gmx.de>
2015-09-26 12:00:46 +0200

    mobile zoom disabled

--- resources/views/layouts/clean.blade.php
+++ resources/views/layouts/clean.blade.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title>Utdela - @yield('title')</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
         <link rel="stylesheet" href="{{asset('css/bootstrap-theme.min.css')}}">
         <script src="{{asset('js/jquery.min.js')}}"></script>
